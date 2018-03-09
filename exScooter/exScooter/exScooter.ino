f7ac2464509b46ba4eac0d27938d69cd684e196d
Maciek-exPC <Maciek@DESKTOP-exPC>
2018-03-09 22:30:26 +0100

    create blank solution

---
+++ exScooter/exScooter/exScooter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		exScooter.ino
+ Created:	3/9/2018 10:30:09 PM
+ Author:	Maciek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
